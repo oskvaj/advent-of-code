02c08397ffb24a2d93066451c5d80e0da8fcceb4
oskvaj <oskarvajlok@gmail.com>
2025-12-08 15:18:21 +0100

    Added 2025 day 8a

---
+++ 2025/day8/day8a.py
@@ -0,0 +1,65 @@
+import time
+from queue import PriorityQueue
+
+
+def dist(point1: tuple[int, int, int], point2: tuple[int, int, int]) -> int:
+    return (
+        (point1[0] - point2[0]) ** 2
+        + (point1[1] - point2[1]) ** 2
+        + (point1[2] - point2[2]) ** 2
+    )
+
+
+start_time = time.time()
+with open("2025/day8/day_8_input.txt", "r") as file:
+    raw_data = file.read().strip().splitlines()
+
+data = [tuple(int(x) for x in row.split(",")) for row in raw_data]
+q = PriorityQueue()
+for i in range(len(data)):
+    for j in range(i + 1, len(data)):
+        q.put((dist(data[i], data[j]), data[i], data[j]))
+
+networks: dict[int, list[tuple[int, int, int]]] = {}
+poles_to_networks: dict[tuple[int, int, int], int] = {}
+network_identifiers: int = 1
+for i in range(1000):
+    d, point1, point2 = q.get()
+    if point1 not in poles_to_networks and point2 not in poles_to_networks:
+        # none of them are in a network
+        networks[network_identifiers] = [point1, point2]
+        poles_to_networks[point1] = network_identifiers
+        poles_to_networks[point2] = network_identifiers
+        network_identifiers += 1
+    elif point1 in poles_to_networks and point2 not in poles_to_networks:
+        # point 1 in a network, but 2 isn't
+        networks[poles_to_networks[point1]].append(point2)
+        poles_to_networks[point2] = poles_to_networks[point1]
+    elif point1 not in poles_to_networks and point2 in poles_to_networks:
+        # point 1 not in a network, but 2 is
+        networks[poles_to_networks[point2]].append(point1)
+        poles_to_networks[point1] = poles_to_networks[point2]
+    else:
+        # both are in separate networks
+        p1id = poles_to_networks[point1]
+        p2id = poles_to_networks[point2]
+        if p1id == p2id:
+            continue
+        affected_poles = networks[p2id]
+        networks[p1id].extend(affected_poles)
+        networks.pop(p2id)
+        for pole in affected_poles:
+            poles_to_networks[pole] = p1id
+
+network_sizes = []
+for value in networks.values():
+    network_sizes.append(len(value))
+
+network_sizes.sort(reverse=True)
+
+print(network_sizes[0] * network_sizes[1] * network_sizes[2])
+
+end_time = time.time()
+print(
+    f"Time took: {round(end_time - start_time)}sec and {round((round((end_time - start_time) * 1000, 2))%1000.0, 2)}ms"
+)
