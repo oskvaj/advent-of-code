b19a4427ff6778b47a0713ad917135777f17c649
oskvaj <oskarvajlok@gmail.com>
2025-12-10 09:00:51 +0100

    2025 day 10 a

---
+++ 2025/day10/day10a.py
@@ -0,0 +1,64 @@
+import time
+
+start_time = time.perf_counter()
+with open("2025/day10/day_10_input.txt", "r") as file:
+    raw_data = file.read().strip().splitlines()
+
+data = [tuple(x for x in item.split()) for item in raw_data]
+goals = []
+button_list = []
+joltages = []
+
+for item in data:
+    goals.append([0 if char == "." else 1 for char in item[0][1:-1]])
+    button_list.append(
+        [list(map(int, button[1:-1].split(","))) for button in item[1:-1]]
+    )
+    joltages.append(list(map(int, item[-1][1:-1].split(","))))
+
+
+def find_shortest_combo(
+    start: list[int], buttons: list[list[int]], depth: int
+) -> list[list[int]]:
+    if depth > 1:
+        returned_results = find_shortest_combo(start, buttons, depth - 1)
+        results = []
+        for result in returned_results:
+            for button in buttons:
+                temp = result.copy()
+                for digit in button:
+                    temp[digit] = (temp[digit] + 1) % 2
+                results.append(temp)
+        return results
+    else:
+        results = []
+        for button in buttons:
+            temp = start.copy()
+            for digit in button:
+                temp[digit] = (temp[digit] + 1) % 2
+            results.append(temp)
+        return results
+
+
+smallest_combination_sum = 0
+j = 0
+for goal, buttons, joltages in zip(goals, button_list, joltages):
+    print(round(j * 100 / len(goals), 2), "%  done")
+    i = 1
+    while True:
+        print("checking depth", i)
+        results = find_shortest_combo([0 for _ in goal], buttons, i)
+        if goal in results:
+            smallest_combination_sum += i
+            break
+        else:
+            i += 1
+    j += 1
+
+
+print(smallest_combination_sum)
+
+end_time = time.perf_counter()
+print(
+    f"Time took: {round(end_time - start_time)}sec and {round((round((end_time - start_time) * 1000, 2))%1000.0, 2)}ms"
+)
